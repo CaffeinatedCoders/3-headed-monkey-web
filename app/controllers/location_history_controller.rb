523d8ab813d989294cb3b8cee31808a6372cbc5d
xorrizon <xorrizon@gmail.com>
2014-11-26 13:34:58 +0100

    Fixed scoping in LocationHistoryController

--- app/controllers/location_history_controller.rb
+++ app/controllers/location_history_controller.rb
@@ -1,10 +1,16 @@
 class LocationHistoryController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @devices = current_user.devices
   end
 
   def show
-    @device = Device.find params[:id]
+    if current_user.admin?
+      @device = Device.find params[:id]
+    else
+      @device = current_user.devices.find params[:id]
+    end
     respond_to do |format|
       format.json {render json: @device.locations}
     end
