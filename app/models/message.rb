2c080b8463611fc3c87deba5cc98afb457f5f9db
Daniel Pail <daniel.pail@student.tugraz.at>
2015-02-06 14:56:38 +0100

    Added html output for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ActiveRecord::Base
   belongs_to :device
   default_scope { order(created_at: :asc) }
+
+  def text_html
+    t = ERB::Util.html_escape text
+    ApplicationController.helpers.simple_format t
+  end
+
 end
