f5043a23e0d86153e0989ac87ffb75c417bb0829
Daniel Pail <daniel.pail@student.tugraz.at>
2015-02-11 14:32:45 +0100

    Now showing original filename in file uploaded message

--- app/controllers/api/v1/uploaded_files_controller.rb
+++ app/controllers/api/v1/uploaded_files_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UploadedFilesController < Api::V1::BaseController
   def create
     @uploaded_file = @device.uploaded_files.create uf_params
     if @uploaded_file.errors.empty?
-      @device.messages.create(text: "New file uploaded: #{view_context.link_to @uploaded_file.filename, device_uploaded_file_path(@device, @uploaded_file)}")
+      @device.messages.create(text: "New file uploaded: #{view_context.link_to @uploaded_file.original_filename, device_uploaded_file_path(@device, @uploaded_file)}")
       head :ok
     else
       render json: @uploaded_file.errors, status: :unprocessable_entity
